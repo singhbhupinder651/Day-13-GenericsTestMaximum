194183d20b863694ae3829ad9783291303d504b7
singhbhupinder651 <71844883+singhbhupinder651@users.noreply.github.com>
2022-11-04 20:45:34 +0530

    updated

--- FindMax.cs
+++ FindMax.cs
@@ -6,35 +6,60 @@ using System.Threading.Tasks;
 
 namespace FindMaximum
 {
-    internal class FindMax<T> where T : IComparable
+    internal class FindMax
     {
-        public T[] Value;
+        public int FindMaxInteger(int FirstNum, int SecondNum, int ThirdNum)
+        {
 
-        public FindMax(params T[] value)
-        {
-            this.Value = value;
-        }
-        public T[] Sort(T[] values)//Method to sort Array Element
-        {
-            Array.Sort(values);
-            return values;
-        }
-        public T MaxValue(T[] values)
-        {
-            var sorted_Values = Sort(values);
-            return sorted_Values[^1];
-        }
-        public T Maxmethod()
-        {
-            var max = MaxValue(this.Value);
-            return max;
-        }
-        public void PrintMaxValue()
-        {
-            var max = MaxValue(this.Value);
-            Console.WriteLine("Maximum Value is :  " + max);
+            if (FirstNum.CompareTo(SecondNum) > 0 && FirstNum.CompareTo(ThirdNum) > 0)
+            {
+                return FirstNum;
+            }
+            if (SecondNum.CompareTo(FirstNum) > 0 && SecondNum.CompareTo(ThirdNum) > 0)
+            {
+                return SecondNum;
+            }
+            if (ThirdNum.CompareTo(FirstNum) > 0 && ThirdNum.CompareTo(SecondNum) > 0)
+            {
+                return ThirdNum;
+            }
+            return 0;
+
         }
 
-    }
+        internal float FindMaxFloat(float Value1, float Value2, float Value3)
+        {
 
+            if (Value1.CompareTo(Value2) > 0 && Value1.CompareTo(Value3) > 0)
+            {
+                return Value1;
+            }
+            if (Value2.CompareTo(Value1) > 0 && Value2.CompareTo(Value3) > 0)
+            {
+                return Value2;
+            }
+            if (Value3.CompareTo(Value1) > 0 && Value3.CompareTo(Value2) > 0)
+            {
+                return Value3;
+            }
+            return 0;
+        }
+
+        internal string FindMaxString(string s1, string s2, string s3)
+        {
+            if (s1.CompareTo(s2) > 0 && s1.CompareTo(s3) > 0)
+            {
+                return s1;
+            }
+            if (s2.CompareTo(s1) > 0 && s2.CompareTo(s3) > 0)
+            {
+                return s2;
+            }
+            if (s3.CompareTo(s1) > 0 && s3.CompareTo(s2) > 0)
+            {
+                return s3;
+            }
+            return default;
+
+        }
 }
