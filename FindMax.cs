60349679357713f414c562f95393d37366dcf71f
singhbhupinder651 <71844883+singhbhupinder651@users.noreply.github.com>
2022-11-04 10:06:43 +0530

    updated

---
+++ FindMax.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximum
+{
+    internal class FindMax<T> where T : IComparable
+    {
+        public T[] Value;
+
+        public FindMax(params T[] value)
+        {
+            this.Value = value;
+        }
+        public T[] Sort(T[] values)//Method to sort Array Element
+        {
+            Array.Sort(values);
+            return values;
+        }
+        public T MaxValue(T[] values)
+        {
+            var sorted_Values = Sort(values);
+            return sorted_Values[^1];
+        }
+        public T Maxmethod()
+        {
+            var max = MaxValue(this.Value);
+            return max;
+        }
+        public void PrintMaxValue()
+        {
+            var max = MaxValue(this.Value);
+            Console.WriteLine("Maximum Value is :  " + max);
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FindMaximum
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {     
+            //maximum integer
+            FindMax<int> obj = new FindMax<int>(22, 54, 62, 87, 32);
+            obj.PrintMaxValue();
+
+            //maximum float
+            FindMax<float> objf = new FindMax<float>(22.6f, 122.8f, 22.6f, 65.6f, 65.9f);
+            objf.PrintMaxValue();
+
+            //find maximum string
+            FindMax<string> objs = new FindMax<string>("zApple", "Peach", "Banana", "Mango", "Stewberry");
+            objs.PrintMaxValue();
+
+        }
+    }
+}
